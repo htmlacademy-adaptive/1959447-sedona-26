f30cbf99df36eba9ccc1f4044e64944abb3ed4c4
Keks <keks@htmlacademy.ru>
2022-08-20 15:09:20 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,6 +1,40 @@
-let menu = document.querySelector('.header');
-let openMenuButton = document.querySelector('.toggle');
+const menu = document.querySelector(".header");
+const openMenuButton = document.querySelector(".toggle");
 
-openMenuButton.addEventListener('click', function () {
-  menu.classList.toggle('header--opened');
+openMenuButton.addEventListener("click", function () {
+  menu.classList.toggle("header--opened");
 });
+
+const map = L.map("map").setView(
+  {
+    lat: 36.6205,
+    lng: 29.1141,
+  },
+  10
+);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+
+const mainPinIcon = L.icon({
+  iconUrl: '../img/icon/map-marker.svg',
+  iconSize: [27, 27],
+  iconAnchor: [27, 13],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 36.6205,
+    lng: 29.1141,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
