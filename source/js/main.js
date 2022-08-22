508e4c0f33435c205975ed230fe2995bcc9f3162
Elena <pumpkinpum@gmail.com>
2022-08-22 14:36:59 +0300

    Add counter

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,20 @@
 const menu = document.querySelector(".header");
 const openMenuButton = document.querySelector(".toggle");
+const heart = document.querySelector(".button-love");
+const likesNumber = document.querySelector(".card__like-count");
 
 openMenuButton.addEventListener("click", function () {
   menu.classList.toggle("header--opened");
 });
+
+heart.onclick = function () {
+  if (heart.classList.contains("button-love--like")) {
+    likesNumber.textContent--;
+  } else {
+    likesNumber.textContent++;
+  }
+  heart.classList.toggle("button-love--like");
+};
 
 const map = L.map("map").setView(
   {
@@ -13,15 +24,13 @@ const map = L.map("map").setView(
   10
 );
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
 }).addTo(map);
 
-
-
 const mainPinIcon = L.icon({
-  iconUrl: 'img/icon/map-marker.svg',
+  iconUrl: "img/icon/map-marker.svg",
   iconSize: [27, 27],
   iconAnchor: [27, 13],
 });
@@ -34,7 +43,7 @@ const mainPinMarker = L.marker(
   {
     draggable: true,
     icon: mainPinIcon,
-  },
+  }
 );
 
 mainPinMarker.addTo(map);
